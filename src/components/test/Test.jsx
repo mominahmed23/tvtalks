6af183f1b8f642936e59e51c25838ef0f7074857
Momin Ahmed <mominahmad324@gmail.com>
2020-05-02 00:50:12 +0500

    jsx

---
+++ src/components/test/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Test = () => {
+    return (
+        <div className="test-div mt-5 text-center">
+            <p>hello testing</p>
+            <div>ho</div>
+            <p className="hello"></p>
+            <input className="btn btn-primary" type="button" value="yo"/>
+        </div>
+    )
+}
