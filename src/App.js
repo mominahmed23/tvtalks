78ed709ca5129ff2dae85671b1ad065b1bdbbb99
Momin Ahmed <mominahmad324@gmail.com>
2020-05-02 00:33:25 +0500

    shows data structure

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import './styling/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
-
+import {data} from "./tvshowsData";
 
 
 function App() {
+  console.log(data);
   return (
     <Container className="test">
       <h1 className="text-center">TVtalks Project</h1>
---
+++ src/tvshowsData.js
@@ -0,0 +1,10 @@
+export const data = [
+    {
+        id: 1,
+        image:"https://i.pinimg.com/originals/d4/65/2d/d4652d0351fd059977785a8f7a14e400.jpg",
+        title:"Peaky Blinders",
+        runStart : 2010,
+        runEnd: null,
+        rating: 90
+    },
+];
